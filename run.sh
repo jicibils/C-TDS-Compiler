74d094c8f62c1d86c1f19029d995872662b3b84f
jicibils <jicibils@hotmail.com>
2016-09-05 13:52:33 -0300

    updated hierarchy folders and updated run.sh

--- run.sh
+++ run.sh
@@ -8,8 +8,9 @@ if [ ! -d target/classes ]; then
 	mkdir target
 	mkdir target/classes
 fi
-java -jar lib/jflex-1.6.1.jar -d src/main/java/ src/main/jflex/lexer.flex
-java -jar lib/java-cup-11b.jar -destdir src/main/java/ -parser Parser -symbols Sym src/main/cup/parser.cup
+java -jar lib/jflex-1.6.1.jar -d src/main/java/scanner/ src/main/jflex/lexer.flex
+java -jar lib/jflex-1.6.1.jar -d src/main/java/scanner/ src/main/jflex/lexerStandalone.flex
+java -jar lib/java-cup-11b.jar -destdir src/main/java/parser/ -parser Parser -symbols Sym src/main/cup/parser.cup
 
 # echo "*******************  COLLECTING DEPENDENCIES  *********************************"
 # mvn dependency:copy-dependencies
@@ -18,6 +19,6 @@ for file in `ls lib`; do export CLASSPATH=$CLASSPATH:lib/$file; done
 export CLASSPATH=$CLASSPATH:target/classes
 echo "*******************  COMPILING PROGRAM******************************************"
 
-javac -d target/classes/ src/main/java/*.java src/main/*.java -classpath $CLASSPATH
+javac -d target/classes/ src/main/java/parser/*.java src/main/java/scanner/*.java src/main/*.java -classpath $CLASSPATH
 
 
