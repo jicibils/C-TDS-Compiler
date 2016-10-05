051b32153efea2a1d80a6a93ba17576365b0405d
Ezequiel Arangue <eze.arangue@gmail.com>
2016-10-05 19:54:24 -0300

    Add ErrorClass. This class represents an error for use in the visitors

---
+++ src/main/java/ast/ErrorClass.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.ast;
+
+/**
+ *
+ * @author Ezequiel Arangue
+ */
+public class ErrorClass {
+    String desc;
+    int line;
+    int col;
+    
+    public ErrorClass(int l, int c, String d){
+        this.desc = d;
+        this.line = l;
+        this.col  = c;
+    }
+    public String getDesc(){
+        return desc;
+    }
+    
+    public void setDesc(String d){
+        this.desc = d;
+    }
+    
+    public int getLine(){
+        return line;
+    }
+    
+    public void setLine(int l){
+        this.line = l;
+    }
+
+    public int getColumn(){
+        return col;
+    }
+    
+    public void setColumn(int c){
+        this.col = c;
+    }
+}
